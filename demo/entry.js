046a7cda9a61734084756681d14aa47a312885b9
tehandyb <tehandyb@gmail.com>
2017-06-04 18:51:25 -0400

    demo rollup bundle is working correctly with react

---
+++ demo/entry.js
@@ -0,0 +1,7 @@
+import React from 'react' // Needed for JSX to compile properly
+import ReactDOM from 'react-dom'
+
+ReactDOM.render(
+    <div>test rendering</div>,
+    document.getElementById('entry')
+)
--- rollup.demo.config.js
+++ rollup.demo.config.js
@@ -4,13 +4,14 @@ import nodeResolve from 'rollup-plugin-node-resolve'
 import uglify from 'rollup-plugin-uglify'
 import replace from 'rollup-plugin-replace'
 
+// Bundle using an entry point as if it was a real app using the choropleth
 export default {
-    entry: 'src/choropleth.js',
-    dest: 'build/choropleth-dist.js',
+    entry: 'demo/entry.js',
+    dest: 'demo/demo.js',
     format: 'umd',
     plugins: [
         babel({
-            exclude: 'node-modules/**'
+            exclude: 'node_modules/**'
         }),
         nodeResolve({
             jsnext: true
@@ -19,7 +20,7 @@ export default {
             include: 'node_modules/**',
         }),
         replace({
-            'process.env.NODE_ENV': JSON.stringify('production')
+            'process.env.NODE_ENV': JSON.stringify('development')
         }),
     ],
     sourceMap: true
